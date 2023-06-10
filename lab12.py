25a00af19ee681d9d89446fedc044bfa8771ae18
Eduardo Migueis <edmigueis@hotmail.com>
2023-06-10 14:21:57 -0300

    Lab 12 part 2

--- lab12.py
+++ lab12.py
@@ -3,16 +3,17 @@ from dataclasses import dataclass, field
 
 @dataclass
 class Card():
-    """
-    Represents a single card
-    """
+    """Represents a single card"""
     name: str
     power: int
     suit: str
-    
+
     def get_naming(self) -> str:
         return self.name + self.suit
-    
+
+    def get_comparable(self) -> tuple:
+        return (self.power, self.suit, self.name)
+
     @staticmethod
     def get_power(card: str) -> int:
         lst = ["A", "2", "3", "4", "5", "6",
@@ -22,128 +23,218 @@ class Card():
                 return i
         return -1
 
+    @staticmethod
+    def get_suit_power(suit: str) -> int:
+        lst = ["O", "E", "C", "P"]
+        for i in range(len(lst)):
+            if lst[i] == suit:
+                return i
+        return -1
+
+    def compare_to(self, other) -> int:
+        """
+        Compares first by power than by suit
+        Negative if smaller, positive if greater and zero if equal
+        """
+        if self.power != other.power:
+            return self.power - other.power
+        return Card.get_suit_power(self.suit) - Card.get_suit_power(other.suit)
+
 
 @dataclass
 class CardDeck():
-    """
-    Represents a card deck with multiple cards
-    """
+    """Represents a card deck with multiple cards"""
     cards: list[Card]
-    
+    played_cards: list[Card] = field(default_factory=list)
+
     def add(self, card: Card) -> None:
         # add and keep order by power
         index = 0
-        while index < len(self.cards) and self.cards[index].power < card.power:
+        while (index < len(self.cards)
+               and (self.cards[index].power > card.power
+                    or (self.cards[index].power == card.power
+                    and Card.get_suit_power(self.cards[index].suit)
+                    > Card.get_suit_power(card.suit)))):
             index += 1
         self.cards.insert(index, card)
-    
+
+    def add_played_card(self, card: Card) -> None:
+        self.played_cards.append(card)
+
+    def sort_played_cards(self) -> None:
+        n = len(self.played_cards)
+        for i in range(n - 1):
+            for j in range(0, n - i - 1):
+                card = self.played_cards[j]
+                next_card = self.played_cards[j + 1]
+                is_greater = card.power > next_card.power
+                has_same_power = card.power == next_card.power
+                greater_suit = Card.get_suit_power(
+                    card.suit) > Card.get_suit_power(next_card.suit)
+                if is_greater or (has_same_power and greater_suit):
+                    self.played_cards[j], self.played_cards[
+                        j + 1] = next_card, card
+
     def remove(self, card: Card) -> None:
         self.cards.remove(card)
 
-    def get_cards_to_beat(self, card: Card) -> list:
-        ret = []
-        for beat_opt in self.cards:
-            if beat_opt.power >= card.power:
-                ret.append(beat_opt)
-        return ret
+    def get_cards_to_beat(self, card: Card) -> tuple:
+        """
+        Gets the card that beats current pile top,
+        and if not possible lies in game
+        """
+        counts: dict = {}
+        for i in range(len(self.cards) - 1, -1, -1):
+            counts.setdefault(self.cards[i].power, []).append(self.cards[i])
+
+        for power, cards in counts.items():
+            if power >= card.power:
+                return (cards, cards)
+
+        for power, cards in counts.items():
+            if cards != []:
+                return (cards, [Card(card.name, card.power, c.suit)
+                                for c in cards])
+
+        return ()
 
     def is_empty(self) -> bool:
         return len(self.cards) == 0
 
-    def card_index(self, card) -> int:
+    def was_card_used(self, card: Card) -> bool:
+        """Verifies if a card was used using binary search"""
         left = 0
-        right = len(self.cards) - 1
+        right = len(self.played_cards) - 1
+
         while left <= right:
             mid = (left + right) // 2
-
-            if self.cards[mid] == card:
-                return mid
-            elif self.cards[mid] < card:
+            comparison = self.played_cards[mid].compare_to(card)
+            if comparison == 0:
+                return True
+            elif comparison < 0:
                 left = mid + 1
             else:
                 right = mid - 1
-        return -1
+
+        return False
 
 
 @dataclass
 class Player():
-    """
-    Represents a single player
-    """
+    """Represents a single player"""
     card_deck: CardDeck
 
-    def play(self, last_played: Card) -> list[Card]:
-        if last_played == None:
+    def play(self, last_played: Card) -> tuple:
+        if last_played is None:
             played_cards = self.card_deck.get_cards_to_beat(Card("", 0, ""))
         else:
             played_cards = self.card_deck.get_cards_to_beat(last_played)
-        for card in played_cards:
+        for card in played_cards[0]:
             self.card_deck.remove(card)
+        for card in played_cards[0]:
+            self.card_deck.add_played_card(card)
         return played_cards
 
-    def buy_cards(self, cards: list[Card]):
+    def buy_cards(self, cards: list[Card]) -> None:
         for card in cards:
             self.card_deck.add(card)
 
-    def doubt(self, played_cards) -> bool:
+    def doubt(self, played_cards: list[Card]) -> bool:
+        """
+        Used to verify if the doubt raised by another player verifies or not
+        Returns True if all cards match and False otherwise
+        """
+        self.card_deck.sort_played_cards()
         for card in played_cards:
-            if self.card_deck.card_index(card) == -1:
+            if not self.card_deck.was_card_used(card):
                 return False
         return True
 
     def print_cards(self) -> None:
-        print("Mão: " + " ".join([card.get_naming() for card in self.card_deck.cards]))
+        print(
+            f"Mão:{' ' if not self.card_deck.is_empty() else ''}{' '.join([card.get_naming() for card in self.card_deck.cards])}")
 
 
 @dataclass
 class Game():
-    """
-    Represents the main game
-    """
+    """Represents the main game"""
     players: list[Player]
     doubt_interval: int
     stack: list[Card] = field(init=False)
 
-    def play(self) -> list[Card]:
+    def play(self) -> None:
+        """Main loop(runs until someone wins)"""
         self.stack = []
         num_of_plays = 0
-        for i in range(len(self.players)):
-            if self.someboy_won():
-                break
-            self.print_status()
-            played_cards = self.players[i].play(None if len(self.stack) == 0 else self.stack[-1])
-            print(f"[Jogador {i + 1}] {len(played_cards)} carta(s) {played_cards[0].name}")
-            for card in played_cards:
-                self.stack.append(card)
-            if num_of_plays + 1 % self.doubt_interval == 0:
-                if not self.player[i].doubt():
-                    self.player[i].buy_cards(self.stack)
-            if i == len(self.players) - 1:
-                i = 0
+        print_players = True
+        curr = 0
+
+        while self.get_winner() == -1:
+            self.print_status(players=print_players)
+            print_players = False
+
+            played_cards = self.players[curr].play(
+                self.stack[-1][1] if self.stack else None)
+            num_played_cards = len(played_cards[1])
+            print(
+                f"[Jogador {curr + 1}] {num_played_cards} carta(s) {played_cards[1][0].name}")
+            self.stack.extend(zip(*played_cards))
+
+            if (num_of_plays + 1) % self.doubt_interval == 0:
+                self.print_status(players=False)
+                next_player_index = curr + 2 if curr + \
+                    2 <= len(self.players) else 1
+                print(f"Jogador {next_player_index} duvidou.")
+                if not self.players[curr].doubt(played_cards[1]):
+                    player_to_buy = curr
+                else:
+                    player_to_buy = curr + \
+                        1 if curr < len(self.players) - 1 else 0
+
+                self.players[player_to_buy].buy_cards(
+                    [card[0] for card in self.stack[:-num_played_cards]]
+                    + played_cards[0])
+                self.stack = []
+                print_players = True
+
+            curr = (curr + 1) % len(self.players)
             num_of_plays += 1
 
-    def someboy_won(self):
-        return any([player for player in self.players if player.card_deck.is_empty()])
+        self.print_status(players=print_players)
+        winner = self.get_winner()
+        print(f"Jogador {winner} é o vencedor!")
 
-    def print_status(self):
-        for i in range(len(self.players)):
-            print("Jogador " + str(i + 1))
-            self.players[i].print_cards()
-        print("Pilha: " + " ".join([card.get_naming() for card in self.stack]))
+    def get_winner(self):
+        """Winner has no cards left"""
+        for i, player in enumerate(self.players, 1):
+            if player.card_deck.is_empty():
+                return i
+        return -1
+
+    def print_status(self, players=False):
+        if players:
+            for i, player in enumerate(self.players):
+                print(f"Jogador {i + 1}")
+                player.print_cards()
+
+        stack_cards = [card[0].get_naming() for card in self.stack]
+        print(f"Pilha:{' ' if self.stack else ''}{' '.join(stack_cards)}")
 
 
 def main():
-    J = int(input())
     players = []
-    for _ in range(J):
+    for _ in range(int(input())):
         card_deck = CardDeck([])
         hand = input().split(", ")
         for card in hand:
-            card_deck.add(Card(card[0], Card.get_power(card[0]), card[1]))
+            if len(card) == 3:
+                name, suit = card[0] + card[1], card[2]
+            else:
+                name, suit = card[0], card[1]
+            card_deck.add(Card(name, Card.get_power(name), suit))
         players.append(
             Player(card_deck))
-    N = int(input())
-    Game(players, N).play()
+    Game(players, int(input())).play()
 
 
 if __name__ == "__main__":
